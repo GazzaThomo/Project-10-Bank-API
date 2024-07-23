04054821c116c1a5acaeb2fb66ee51084f6b4cec
Gareth Thomas <thomasgareth3@gmail.com>
2024-07-23 11:53:35 +0200

    scss change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,14 +4,17 @@ import HomePage from "./Pages/HomePage";
 import SignInPage from "./Pages/SignInPage";
 import UserPage from "./Pages/UserPage";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/sign-in" element={<SignInPage />} />
-      <Route path="/user" element={<UserPage />} />
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-in" element={<SignInPage />} />
+        <Route path="/user" element={<UserPage />} />
+      </Routes>
+    </>
   );
 }
 
